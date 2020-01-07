f07fa062cd641c933896afcfbcb09aaa5f956094
SaltyChiang <jxyzjchina@gmail.com>
2020-01-08 00:01:03 +0800

    Correct formule of Normal Distribution.

--- testrandom.py
+++ testrandom.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 volume = 16**3 * 16
 data = np.fromfile('data/random.data').reshape(volume, 8)
 data0 = np.around(data[:, 0] * 1000).astype('int')
-print(data0.shape)
-xi = 1.0
+sigma2 = 1.0
 x = np.arange(-5000, 5001) / 1000
-y1 = np.exp(-0.5 * np.power(x / xi, 2))
+y1 = np.exp((-0.5 / sigma2) * np.power(x, 2)) / \
+     np.math.sqrt(2 * np.math.pi * sigma2)
 y2 = np.zeros(10001)
+print(data0.shape)
 for i in range(volume):
     if data0[i] >= -5000 and data0[i] < 5001:
         y2[data0[i] + 5000] += 1
-y2 /= volume / 1000 / np.math.sqrt(2 * np.math.pi)
+y2 /= volume / 1000
 plt.plot(x, y1, 'b,-')
 plt.bar(x, y2, 1 / 1000)
 plt.show()
