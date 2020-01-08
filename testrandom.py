f175a0cb73d3faa13783b05ada2d0a892e3bcc86
SaltyChiang <jxyzjchina@gmail.com>
2020-01-08 13:31:20 +0800

    numba added but unnoticeable.

--- testrandom.py
+++ testrandom.py
@@ -3,25 +3,28 @@ from numba import jit
 import matplotlib.pyplot as plt
 
 volume = 16**3 * 16
-data = np.fromfile('data/random.data').reshape(volume, 8)
-data0 = np.around(data[:, 0] * 1000).astype('int')
 sigma2 = 1.0
-x = np.arange(-5000, 5001) / 1000
+sigma_range = int(7)
+density = int(50)
+
+left = -sigma_range * density
+right = sigma_range * density + int(1)
+data = np.fromfile('data/random.data').reshape(volume, 8)
+data0 = np.around(data[:, 0] * density).astype('int')
+x = np.arange(left, right) / density
 y1 = np.exp((-0.5 / sigma2) * np.power(x, 2)) / \
      np.math.sqrt(2 * np.math.pi * sigma2)
-y2 = np.zeros(10001)
-print(np.size(data0, 0))
+y2 = np.zeros(right - left)
 
 
-@jit
 def fill(sample, y):
     for i in range(sample.size):
-        if sample[i] >= -5000 and sample[i] < 5001:
-            y[sample[i] + 5000] += 1
-    y /= sample.size / 1000
+        if sample[i] >= left and sample[i] < right:
+            y[sample[i] - left] += 1
+    y /= sample.size / density
 
 
 fill(data0, y2)
 plt.plot(x, y1, 'b,-')
-plt.bar(x, y2, 1 / 1000)
+plt.bar(x, y2, 1 / density)
 plt.show()
