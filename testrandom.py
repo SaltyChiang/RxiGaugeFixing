c845c39876d2c1697a2b2ef78071cb9d3d77b47a
SaltyChiang <jxyzjchina@gmail.com>
2020-01-08 11:03:11 +0800

    numba added but unnoticeable.

--- testrandom.py
+++ testrandom.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import jit
 import matplotlib.pyplot as plt
 
 volume = 16**3 * 16
@@ -9,11 +10,18 @@ x = np.arange(-5000, 5001) / 1000
 y1 = np.exp((-0.5 / sigma2) * np.power(x, 2)) / \
      np.math.sqrt(2 * np.math.pi * sigma2)
 y2 = np.zeros(10001)
-print(data0.shape)
-for i in range(volume):
-    if data0[i] >= -5000 and data0[i] < 5001:
-        y2[data0[i] + 5000] += 1
-y2 /= volume / 1000
+print(np.size(data0, 0))
+
+
+@jit
+def fill(sample, y):
+    for i in range(sample.size):
+        if sample[i] >= -5000 and sample[i] < 5001:
+            y[sample[i] + 5000] += 1
+    y /= sample.size / 1000
+
+
+fill(data0, y2)
 plt.plot(x, y1, 'b,-')
 plt.bar(x, y2, 1 / 1000)
 plt.show()
